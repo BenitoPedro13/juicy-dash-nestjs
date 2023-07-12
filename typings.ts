2f2f08227d9183ecdce68a254ef00c8af7409d7b
Benito VPS <benitopedro21@outlook.com>
2023-07-12 07:59:04 +0000

    wip

--- typings.ts
+++ typings.ts
@@ -1,5 +1,19 @@
 /* eslint-disable */
 export type Schema = {
+  '_prisma_migrations': {
+    plain: {
+      'id': string;
+      'checksum': string;
+      'finished_at': string;
+      'migration_name': string;
+      'logs': string;
+      'rolled_back_at': string;
+      'started_at': string;
+      'applied_steps_count': number;
+    };
+    nested: {};
+    flat: {};
+  };
   'Category': {
     plain: {
       'id': number;
