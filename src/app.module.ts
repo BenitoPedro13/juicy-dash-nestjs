1592859f3ba9d7be818e5c54cf58ed73871c6cd0
Benito Pedro <benitopedro21@outlook.com>
2023-07-17 13:57:25 -0300

    wip file upload & serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CsvsModule } from './csvs/csvs.module';
 // import { MulterModule } from '@nestjs/platform-express';
 import { AttachmentsModule } from './attachments/attachments.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, CsvsModule, AttachmentsModule],
+  imports: [
+    PrismaModule,
+    CsvsModule,
+    AttachmentsModule,
+    ServeStaticModule.forRoot({
+      serveRoot: 'public',
+      rootPath: join(__dirname, '..', '..', 'files'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
