92c310694ef0cb68971449ab49dd0bac23184584
Benito Pedro <benito@juicy.space>
2024-06-21 09:25:26 -0300

    👌 IMPROVE: Added pagination & sorting to `UsersService.findAll`

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { User } from '.prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { sortFields, sortOrder } from 'types/queyParams';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +13,37 @@ export class UsersService {
     return this.prisma.user.create({ data: createUserDto as any });
   }
 
-  async findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  async findAll({
+    start,
+    end,
+    sort,
+    order,
+  }: {
+    start: number;
+    end: number;
+    sort: sortFields<User>;
+    order: sortOrder;
+  }): Promise<User[]> {
+    try {
+      console.log('sort :', sort);
+      console.log('order :', order);
+
+      const orderBy = sort.map((item, index) => {
+        return {
+          [item]: order[index],
+        };
+      });
+
+      const pageSize = end - start;
+
+      return this.prisma.user.findMany({
+        take: pageSize,
+        skip: start,
+        orderBy: orderBy,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async findOne(id: number): Promise<User | null> {
