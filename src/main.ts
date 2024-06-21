f9f0e2887ffe97d022ebacd771dbf188021b7cf1
Benito Pedro <benito@juicy.space>
2024-06-21 09:22:14 -0300

    👌 IMPROVE: changed server listening port to `3001`

--- src/main.ts
+++ src/main.ts
@@ -173,7 +173,7 @@ async function bootstrap() {
       errorHttpStatusCode: 422,
     }),
   );
-  await app.listen(3000);
+  await app.listen(3001);
   logger.verbose('Application started successfully');
 }
 
